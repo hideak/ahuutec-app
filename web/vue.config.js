87a7c14209088a2f6a78ecea39891248ed44841b
Willian Hideak <whideak@gmail.com>
2021-04-14 12:53:22 -0300

    Added Vue config file

---
+++ web/vue.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/camelcase */
+
+// Vue Configuration File
+module.exports = {
+  pwa: {
+    name: 'Ahuutec App',
+    themeColor: '#fafafa',
+    msTileColor: '#fafafa',
+    manifestOptions: {
+      short_name: 'Ahuutec',
+      background_color: '#fafafa'
+    }
+  },
+  configureWebpack: {
+    devtool: 'source-map'
+  }
+};
